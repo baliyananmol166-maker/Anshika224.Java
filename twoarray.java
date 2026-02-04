043d7d7101d0d964d0074603ac86485bd616cdf4
baliyananmol-166 <baliyananmol166@gamil.com>
2026-02-04 11:28:21 +0530

    2-D Array

---
+++ twoarray.java
@@ -0,0 +1,18 @@
+public class twoarray{
+    public static void main(String[] args){
+        int[][] matrix={
+            {1,2,3},
+            {4,5,6}
+        };
+
+        System.out.println("Two Dimensional Array elements are:");
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[i].length;j++){
+
+                System.out.println("Element at ("+i+","+j+") = "+matrix[i][j]);
+            }
+            System.out.println();
+
+        }
+    }
+}
