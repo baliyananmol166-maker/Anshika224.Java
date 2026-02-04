7c50c809c6f45df6bdd1abe5d489f8fd536344bd
baliyananmol-166 <baliyananmol166@gamil.com>
2026-02-04 11:22:30 +0530

    1-D Array

---
+++ Array1.java
@@ -0,0 +1,16 @@
+public class Array1{
+
+    public static void main(String[] args){
+        
+        int[] numbers={10,20,30,40,50};
+
+        System.out.println("Array elements are:");
+        for(int i=0; i<numbers.length; i++){
+
+        System.out.println("Array Elements"+"="+" "+numbers[i]);
+        }
+    }
+
+}
+
+
