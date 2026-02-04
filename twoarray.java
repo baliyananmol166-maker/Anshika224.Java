391e3cd30b283948da27644f0f7fe67474874cb9
baliyananmol-166 <baliyananmol166@gamil.com>
2026-02-04 11:31:16 +0530

    2-D Array

--- twoarray.java
+++ twoarray.java
@@ -9,7 +9,7 @@ public class twoarray{
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[i].length;j++){
 
-                System.out.println("Element at ("+i+","+j+") = "+matrix[i][j]);
+                System.out.println("Element are:" +matrix[i][j]);
             }
             System.out.println();
 
