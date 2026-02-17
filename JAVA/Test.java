7bdf7cbefa6ee511f9a4a36386c1e6f8bbf4de4c
baliyananmol-166 <baliyananmol166@gamil.com>
2026-02-17 09:17:24 +0530

    Unit 2

---
+++ JAVA/Test.java
@@ -0,0 +1,16 @@
+public class Test{
+
+    public static void main(String[] args){
+
+
+        try{
+            int a=10;
+            int b=0;
+            System.out.println(a/b);
+        }catch (Exception e){
+
+            System.out.println("Cannot divide by zero");
+        }
+        System.out.println("program continue.......");
+    }
+}
