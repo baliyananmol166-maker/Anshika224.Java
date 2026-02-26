b5680cb5accbc24f3b5675d3f60b956be69c5823
baliyananmol-166 <baliyananmol166@gamil.com>
2026-02-26 09:43:22 +0530

    Local Variable

---
+++ UNIT3/localvar.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class localvar{
+   public static void main(String[] args) {
+       var name="anshika";
+       var number=100;
+       var list=new ArrayList<String>();
+
+       list.add("java");
+       System.out.println(name);
+       System.out.println(number);
+       System.out.println(list);
+       
+    }
+}
