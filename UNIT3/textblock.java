27171be7a11b595d33a72da4f0974e5ab9079975
baliyananmol-166 <baliyananmol166@gamil.com>
2026-02-26 09:53:23 +0530

    textblock

---
+++ UNIT3/textblock.java
@@ -0,0 +1,12 @@
+public class textblock{
+    public static void main(String[] args){
+        String json="""
+        {
+            "name":"Anshika",
+            "age":18,
+            "city":"Muzaffarnagar"
+        }
+        """;
+        System.out.println(json);
+    }
+}
