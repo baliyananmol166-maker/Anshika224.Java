aba75fea08645da10abca31c15a39aa1815aba39
baliyananmol-166 <baliyananmol166@gamil.com>
2026-02-26 08:59:13 +0530

    Unit3

--- UNIT3/methodreference.java
+++ UNIT3/methodreference.java
@@ -4,11 +4,16 @@ import java.util.Arrays;
 public class methodreference{
    public static void main(String[] args){
      
-     List<String> names = Arrays.asList("A", "B", "C");
+     List<String> names = Arrays.asList("Anshika", "Baliyan", "Choudhary");
      // Using lambda expression
         names.forEach(name -> System.out.println(name));
 
     // Using method reference
-        names.forEach(System.out::println);
+    //without
+    //system.out.println(name);
+    //name(system.out::println);
+    //name.foreach(system.out::println);
+
+        names.forEach(System.out::println);    //classname :: methodreference
    }
 }
