bf3b27e74aec9e540287d7922d2479c40d2d1f35
baliyananmol-166 <baliyananmol166@gamil.com>
2026-03-20 19:51:08 +0530

    Java

---
+++ JAVA/Program35.java
@@ -0,0 +1,13 @@
+//string demo in java...
+public class Program35 {
+    public static void main(String[] args) {
+        String s1 = "Hello world";
+        String s2 = "Hello world";
+        if (s1 == s2) {
+            System.out.println("String points to same object");
+        }
+        else {
+            System.out.println("String do not points to same object");
+        }
+    }
+}
